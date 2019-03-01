d40e30a40dcc5041111ac64f2a3979701ccad7b0
dnpfefferle <47363203+dnpfefferle@users.noreply.github.com>
2019-02-28 19:34:12 -0600

    added dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+#dictionaries use curly brackets
+#unlike lists, dictionaries store info in pairs or multiple pairs
+
+good_boi = {"name": "Rex", 
+            "animal": "dog", 
+            "no_hobbies": 4, 
+            "wakeup_weekend": 10}
+
+print(f'Hello I am {good_boi["name"]} and I am a {good_boi["animal"]}.')
+
+print(f'I have  {good_boi["no_hobbies"]}.')
+
+print(f'On the weekend I get up at  {good_boi["wakeup_weekend"]}.')
+
+#This works, but checked the solved for lists/dictionaries within dictionary
